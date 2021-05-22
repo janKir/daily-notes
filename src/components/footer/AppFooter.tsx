668d0c9844e734e717c32a34e6f0a5d2a1a9b10e
janKir <9143877+janKir@users.noreply.github.com>
2021-05-22 19:26:35 +0200

    add description to footer

--- src/components/footer/AppFooter.tsx
+++ src/components/footer/AppFooter.tsx
@@ -8,11 +8,18 @@ export const AppFooter: React.FC = () => {
         padding: 16,
         fontSize: 11,
         display: "flex",
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         backgroundColor: colors.teaGreen1,
+        color: colors.black,
+        fontStyle: "italic",
       }}
     >
-      created by Jan Kirchner, 2021
+      <span>
+        Be better prepared for your daily stand-up meeting! Keep track of what
+        you've done the day before, what's on your plate for today, and what
+        issues you want to discuss with your team.
+      </span>
+      <span>created by Jan Kirchner, 2021</span>
     </footer>
   );
 };
