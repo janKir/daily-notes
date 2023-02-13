26084a80494b40e11a3daf17f219e562c6e9268b
Jan Kirchner <jan.kirchner@extern.wtl.de>
2023-02-13 02:04:00 +0000

    update vite & linaria

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import vitePluginLinaria from 'vite-plugin-linaria'
+/* eslint-disable import/no-default-export */
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import linaria from "@linaria/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
-    vitePluginLinaria(),
-  ]
+    linaria({
+      include: ["**/*.{ts,tsx}"],
+      babelOptions: {
+        presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      },
+    }),
+  ],
 });
