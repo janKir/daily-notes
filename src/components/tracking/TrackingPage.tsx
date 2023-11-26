cdcabd2577be59c4db4b574801af8b78539f1e9a
Jan Kirchner <jan.kirchner@extern.wtl.de>
2023-11-26 19:35:38 +0000

    add hotkeys for tracking month view

--- src/components/tracking/TrackingPage.tsx
+++ src/components/tracking/TrackingPage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+import { addMonths, subMonths } from "date-fns";
 import { MonthGrid } from "./MonthGrid";
 import { Flex } from "../common/Flex";
 import { DayCell } from "./DayCell";
@@ -7,6 +9,16 @@ import { MonthBar } from "./monthbar/MonthBar";
 
 export const TrackingPage: React.FC = () => {
   const [month, setMonth] = React.useState(new Date());
+
+  useHotkeys("mod+left", () => {
+    setMonth((m) => subMonths(m, 1));
+  });
+  useHotkeys("mod+right", () => {
+    setMonth((m) => addMonths(m, 1));
+  });
+  useHotkeys(["mod+up", "mod+down"], () => {
+    setMonth(() => new Date());
+  });
 
   return (
     <Flex grow align="stretch">
