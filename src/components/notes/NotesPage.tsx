b3840aaef41ae15740dc2ab43c81454e3442cbab
Jan Kirchner <jan.kirchner@extern.wtl.de>
2023-11-26 19:37:04 +0000

    add hotkeys to notes page

--- src/components/notes/NotesPage.tsx
+++ src/components/notes/NotesPage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { ContentContainer } from "./ContentContainer";
 import { useAppContext } from "../../contexts/AppContext";
 import { useGetNextActiveDateOrWithNote } from "../../hooks/useGetNextActiveDateOrWithNote";
 import { DateBar } from "./datebar/DateBar";
 
 export const NotesPage: React.FC = () => {
-  const { date } = useAppContext();
+  const { date, setDate } = useAppContext();
   const getNextActiveDateOrWithNote = useGetNextActiveDateOrWithNote();
 
   const dateAfter = getNextActiveDateOrWithNote(date, false);
   const dateBefore = getNextActiveDateOrWithNote(date, true);
+
+  useHotkeys("mod+left", () => {
+    setDate(dateBefore);
+  });
+  useHotkeys("mod+right", () => {
+    setDate(dateAfter);
+  });
+  useHotkeys(["mod+up", "mod+down"], () => {
+    setDate(() => new Date());
+  });
 
   return (
     <div>
